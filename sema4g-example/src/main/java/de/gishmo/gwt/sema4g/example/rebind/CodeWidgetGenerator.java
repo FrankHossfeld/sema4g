8350f2fef504572521e1a73df7f06746a2b5f838
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-05-20 22:23:10 +0200

    - Remove unnecessary comments - delete the GeneratorUtils class

--- sema4g-example/src/main/java/de/gishmo/gwt/sema4g/example/rebind/CodeWidgetGenerator.java
+++ sema4g-example/src/main/java/de/gishmo/gwt/sema4g/example/rebind/CodeWidgetGenerator.java
@@ -13,23 +13,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
-/*
- * Copyright 2015 Frank Hossfeld
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package de.gishmo.gwt.sema4g.example.rebind;
 
 
@@ -191,7 +174,7 @@ public class CodeWidgetGenerator
                                          typeSpec.build())
                                 .build();
 
-    System.out.println(javaFile.toString());
+//    System.out.println(javaFile.toString());
 
     printWriter.print(javaFile.toString());
     printWriter.flush();
